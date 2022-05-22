52a601b49c3e90f1f0a58fa2f060ab1c6c037070
Gabriel Maia Gondim <41156120+gabrielmaia2@users.noreply.github.com>
2022-05-22 17:58:56 -0300

    Add api key
    Setup default axios url and auth token

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
+import axios from 'axios';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+
+import authToken from './keys/api_key';
+
+axios.defaults.baseURL = 'https://petland-back-end.herokuapp.com/';
+axios.defaults.headers.common.Authorization = authToken;
+axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
