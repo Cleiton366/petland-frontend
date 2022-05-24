d004b6899596b04137990b8031a5fd9408cd6bc1
Gabriel Maia Gondim <41156120+gabrielmaia2@users.noreply.github.com>
2022-05-24 00:01:48 -0300

    Add animal type on search route

--- src/Rotas.jsx
+++ src/Rotas.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {
+  BrowserRouter as Router, Route, Routes,
+} from 'react-router-dom';
 import Search from './Components/Pages/Search/Search';
 
 function Rotas() {
   return (
     <Router>
       <Routes>
-        <Route exact path="/Search" element={<Search />} />
+        <Route path="/search/:animalType" element={<Search />} />
       </Routes>
     </Router>
   );
