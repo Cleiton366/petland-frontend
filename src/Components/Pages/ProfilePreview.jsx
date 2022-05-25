6de2266b0a5e2f31c1d5a611f1ca2eb71720f221
Gabriel Maia Gondim <41156120+gabrielmaia2@users.noreply.github.com>
2022-05-24 22:15:47 -0300

    Use api to get profile preview data

--- src/Components/Pages/ProfilePreview.jsx
+++ src/Components/Pages/ProfilePreview.jsx
@@ -1,15 +1,43 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import ReactLoading from 'react-loading';
 
 import './ProfilePreview.css';
 
 export default function ProfilePreview() {
-  const profilepic = '';
-  const name = 'Bettany Martin';
+  const [user, setUser] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const { data, status } = await axios.get('/user-info');
+
+        if (status >= 200 && status < 300) {
+          setUser(data);
+          setLoading(false);
+        }
+      } catch (err) {
+        console.log(err.message);
+        setLoading(false);
+      }
+    })();
+  }, [user]);
+
+  if (loading) {
+    return (
+      <div className="profile-preview d-flex p-2">
+        <ReactLoading className="align-self-center" width="2em" height="min-content" type="spokes" color="black" />
+      </div>
+    );
+  }
+
+  const { username, avatarurl } = user;
 
   return (
     <div className="profile-preview d-flex p-2">
-      <img className="rounded-circle h-100" src={profilepic || 'https://i.imgur.com/gJmbboJ.png'} alt="Profile preview" />
-      <div className="align-self-center px-2">{name}</div>
+      <img className="rounded-circle h-100" src={avatarurl || 'https://i.imgur.com/gJmbboJ.png'} alt="Profile preview" />
+      {username ? (<div className="align-self-center px-2">{username}</div>) : null}
     </div>
   );
 }
