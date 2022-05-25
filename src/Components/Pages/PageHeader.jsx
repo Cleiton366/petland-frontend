f18d98690d82eb514be7985682ef7d5d4c55a525
Gabriel Maia Gondim <41156120+gabrielmaia2@users.noreply.github.com>
2022-05-24 22:13:02 -0300

    Set header back button icon

--- src/Components/Pages/PageHeader.jsx
+++ src/Components/Pages/PageHeader.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import LineIcon from 'react-lineicons';
 
 import './PageHeader.css';
 import ProfilePreview from './ProfilePreview';
@@ -7,7 +8,9 @@ export default function PageHeader() {
   return (
     <div className="page-header d-flex align-items-center justify-content-between px-5 py-2">
       <div className="d-flex justify-content-start">
-        <button type="button" className="header-back-button">Back</button>
+        <button type="button" className="header-back-button px-4">
+          <LineIcon name="arrow-left" />
+        </button>
       </div>
       <div className="page-header-title align-self-baseline p-0 m-0">Petland</div>
       <div className="d-flex justify-content-end">
