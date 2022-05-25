b6e4a1dcf8204e969dbc1bba844c70d5921bfafc
Gabriel Maia Gondim <41156120+gabrielmaia2@users.noreply.github.com>
2022-05-25 15:52:15 -0300

    Update Search.jsx
    Fix formatting

--- src/Components/Pages/Search/Search.jsx
+++ src/Components/Pages/Search/Search.jsx
@@ -2,11 +2,11 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import ReactLoading from 'react-loading';
 
+import axios from 'axios';
 import style from './Search.module.css';
 import AnimalPreview from './AnimalPreview';
 import PageHeader from '../PageHeader';
 import Container from '../Container2';
-import axios from 'axios';
 
 function Search() {
   const { animalType, searchLocation } = useParams();
@@ -31,16 +31,16 @@ function Search() {
       // }
 
       try {
-        const {data, status} = await axios.get(url, {
+        const { data, status } = await axios.get(url, {
           withCredentials: true,
-        })
+        });
 
         if (status >= 200 && status < 300) {
           setPets(data);
           setLoading(false);
         }
       } catch (err) {
-        console.log(err.message);
+        console.log(err);
         setLoading(false);
       }
     })();
@@ -49,16 +49,16 @@ function Search() {
   useEffect(() => {
     (async () => {
       try {
-        const {data, status} = await axios.get('http://localhost:4000/user-info', {
+        const { data, status } = await axios.get('http://localhost:4000/user-info', {
           withCredentials: true,
-        })
+        });
 
         if (status >= 200 && status < 300) {
           setUser(data);
         }
       } catch (err) {
-        console.log(err.message);
-        setUser({ dummy:null });
+        console.log(err);
+        setUser({ dummy: null });
       }
     })();
   }, []);
