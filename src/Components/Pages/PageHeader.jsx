b0502f99f511a8f7121920ba423c0d951b58c6c7
Gabriel Maia Gondim <41156120+gabrielmaia2@users.noreply.github.com>
2022-05-24 23:05:52 -0300

    Implemented back button

--- src/Components/Pages/PageHeader.jsx
+++ src/Components/Pages/PageHeader.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import LineIcon from 'react-lineicons';
+import { useNavigate } from 'react-router-dom';
 
 import './PageHeader.css';
 import ProfilePreview from './ProfilePreview';
 
 export default function PageHeader() {
+  const navigate = useNavigate();
+
   return (
     <div className="page-header d-flex align-items-center justify-content-between px-5 py-2">
       <div className="d-flex justify-content-start">
-        <button type="button" className="header-back-button px-4">
+        <button type="button" className="header-back-button px-4" onClick={() => navigate(-1)}>
           <LineIcon name="arrow-left" />
         </button>
       </div>
