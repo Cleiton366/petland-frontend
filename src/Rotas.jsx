6530733f0e79b0ea0e9932795987492947ccab82
BergSouza <bergson2000@live.com>
2022-06-29 15:27:14 -0300

    update rotas

--- src/Rotas.jsx
+++ src/Rotas.jsx
@@ -8,6 +8,7 @@ import UserProfile from './Components/Pages/User Profile/UserProfile';
 import Search from './Components/Pages/Search/Search';
 import Donationrequest from './Components/Pages/Donationrequest/Donationrequest';
 import Adopt from './Components/Pages/Adopt/Adopt'
+import DonatePet from './Components/Pages/DonatePet/DonatePet'
 
 function Rotas() {
   return (
@@ -22,6 +23,7 @@ function Rotas() {
         <Route exact path="/Adopt/:id" element={<Adopt />} />
         <Route exact path="/UserProfile" element={<UserProfile />} />
         <Route exact path="/Donationrequest" element={<Donationrequest />} />
+        <Route exact path="/DonatePet" element={<DonatePet />} />
       </Routes>
     </Router>
   )
